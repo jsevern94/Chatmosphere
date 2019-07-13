7c598db7a991b6002123c6de7c966617931db952
Tsige <zigkebede@gmaial.com>
2019-07-13 02:28:04 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const env = require('dotenv').load();
+const exphbs = require('express-handlebars');
+//chat madules
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Passport
+app.use(
+  session({ secret: 'rHUyjs6RmVOD06OdOTsVAyUUCxVXaWci', resave: true, saveUninitialized: true })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Handlebars
+const viewsPath = path.join(__dirname, 'views');
+const layoutsPath = path.join(viewsPath, 'layouts');
+const partialsPath = path.join(viewsPath, 'partials');
+app.set('views', viewsPath);
+
+const exphbsConfig = exphbs.create({
+  defaultLayout: 'main',
+  layoutsDir: layoutsPath,
+  partialsDir: [partialsPath],
+  extname: '.hbs'
+});
+
+app.engine('hbs', exphbsConfig.engine);
+app.set('view engine', '.hbs');
+
+// Models
+const models = require('./models');
+
+// Express static assets
+app.use(express.static("public"));
+
+// Routes
+const authRoute = require('./controllers/auth.js')(app, passport);
+
+// Load passport strategies
+require('./config/passport/passport.js')(passport, models.user);
+
+//socket io connection 
+io.on('connection', function (socket) {
+  console.log('a user connected on socket: ' + socket.id);
+  socket.on('chat message', function (msg) {
+      io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function () {
+      console.log('user disconnected from socket ' + socket.id);
+
+  });
+});
+
+// Sync Database
+models.sequelize
+  .sync()
+  .then(function() {
+    console.log('Database Connected');
+
+    http.listen(3000, function(err) {
+      if (!err) console.log('Connected at http://localhost:3000');
+      else console.log(err);
+    });
+  })
+  .catch(function(err) {
+    console.log(err, 'Error on Database Sync. Please try again!');
+  });
