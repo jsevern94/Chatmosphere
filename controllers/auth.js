8e832f8f9d70adaca186645198438ebedeb54317
jsevern94 <severnj@spu.edu>
2019-07-20 01:44:21 -0700

    more uniform UI, added log in and sign up buttons on landing page, still need to add 'about us' and a few other small changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,6 +25,14 @@ module.exports = (app, passport) => {
     })
   );
 
+  app.get('/signup', (req, res) => {
+    res.render('signup');
+  });
+
+  app.get('/login', (req, res) => {
+    res.render('login');
+  });
+
   app.get('/signup-fail', (req, res) => {
     res.render('signup', {
       message: req.flash('error')
