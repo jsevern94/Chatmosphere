0677415b584085c1c054a59a36c0e403449d24f8
Tsige <zigkebede@gmaial.com>
2019-07-20 00:27:47 -0700

    can you guys check this

--- models/message.js
+++ models/message.js
@@ -1,10 +1,15 @@
 module.exports = (sequelize, Sequelize) => {
-    var Message = sequelize.define("message", {
-        id: { autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER, allowNull: false },
-        sender: { type: Sequelize.STRING, allowNull: false },
-        receiver: { type: Sequelize.STRING, allowNull: false },
-        content: { type: Sequelize.TEXT, allowNull: false }
-    });
+  var Message = sequelize.define("message", {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    sender: { type: Sequelize.STRING, allowNull: false },
+    receiver: { type: Sequelize.STRING, allowNull: false },
+    content: { type: Sequelize.TEXT, allowNull: false }
+  });
 
-    return Message;
+  return Message;
 };
