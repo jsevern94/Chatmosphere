4073529af29ae0132a8d9200f422e21a155e49bb
unknown <archivecoderzero@gmail.com>
2019-07-19 18:00:45 -0700

    updated server.js with http instead of app

--- server.js
+++ server.js
@@ -74,24 +74,6 @@ io.on('connection', function (socket) {
   });
 });
 
-// Sync Database
-// models.sequelize
-//   .sync()
-//   .then(function () {
-//     console.log('Database Connected');
-
-//     http.listen(3000, function (err) {
-//       if (!err) console.log('Connected at http://localhost:3000');
-//       else console.log(err);
-//     });
-//   })
-//   .catch(function (err) {
-//     console.log(err, 'Error on Database Sync. Please try again!');
-
-
-
-
-
 models.sequelize
 .sync()
 .then(function(){
